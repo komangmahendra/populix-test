c5863c153d1c7f4178571f9701f69451ac91e641
komang <komang@farmacare.id>
2023-11-16 18:00:14 +0800

    task: add use list form test

--- src/screen/ListForm/__tests__/useListForm.test.tsx
+++ src/screen/ListForm/__tests__/useListForm.test.tsx
@@ -1,11 +1,17 @@
-import {renderHook} from '@testing-library/react-native';
+import {act, renderHook, waitFor} from '@testing-library/react-native';
 import {configureStore} from '@reduxjs/toolkit';
 import {Provider} from 'react-redux';
+import MockAdapter from 'axios-mock-adapter';
+
 // hooks
 import {useListForm} from '../useListForm';
 
 // redux
 import * as collectionListRedux from '../../../store/collectionList/collectionList';
+import axios from '../../../config/axios';
+import {CollectionListService} from '../../../service/collectionList/collectionListService';
+
+const mockAxios = new MockAdapter(axios);
 
 const mockNavigate = jest.fn();
 const mockParams: {listId: number | null} = {listId: 1};
@@ -22,14 +28,16 @@ const store = configureStore({
   middleware: getDefaultMiddleware => getDefaultMiddleware(),
 });
 
-export const Wrapper = ({children}: any) => (
+const Wrapper = ({children}: any) => (
   <Provider store={store}>{children}</Provider>
 );
 
 const mockToast = jest.fn();
 jest.mock('../../../utils/toast', () => ({
-  toast: mockToast,
+  toast: () => mockToast,
 }));
+mockAxios.onPut(`${CollectionListService.baseURL}/4/list/1`).reply(200);
+mockAxios.onPost(`${CollectionListService.baseURL}/4/list`).reply(200);
 
 let mockGetListById: any;
 describe('Test movie detail hooks', () => {
@@ -42,7 +50,8 @@ describe('Test movie detail hooks', () => {
     renderHook(() => useListForm(), {
       wrapper: Wrapper,
     });
-    expect(mockGetListById).toHaveBeenCalledTimes(0);
+
+    waitFor(() => expect(mockGetListById).toHaveBeenCalledTimes(0));
   });
 
   test('Should call get list detail async when first init with right listId', () => {
@@ -50,21 +59,65 @@ describe('Test movie detail hooks', () => {
     renderHook(() => useListForm(), {
       wrapper: Wrapper,
     });
-    expect(mockGetListById).toHaveBeenCalledWith(1);
+
+    waitFor(() => expect(mockGetListById).toHaveBeenCalledTimes(1));
   });
 
-  // test('Should call mock navigate when call onNavigateToList with right params', () => {
-  //   const {result} = renderHook(() => useListForm(), {
-  //     wrapper: Wrapper,
-  //   });
+  test('Should call api update list when onSave if has listId', async () => {
+    mockParams.listId = 1;
+    let spy = jest.spyOn(axios, 'put');
 
-  //   act(() => {
-  //     result.current.onNavigateToList();
-  //   });
+    const {result} = renderHook(() => useListForm(), {
+      wrapper: Wrapper,
+    });
 
-  //   expect(mockNavigate).toHaveBeenCalledWith('MainList', {
-  //     params: {from: 'MovieDetail', movieId: 1},
-  //     screen: 'List',
-  //   });
-  // });
+    act(() => {
+      result.current.onChangeForm('Test', 'name');
+    });
+
+    await act(async () => {
+      await result.current.onSave();
+    });
+
+    expect(mockGetListById).toHaveBeenCalledWith(1);
+    expect(spy).toHaveBeenCalledWith(
+      `${CollectionListService.baseURL}/4/list/1`,
+      {
+        description: '',
+        iso_3166_1: 'US',
+        iso_639_1: 'en',
+        name: 'Test',
+        public: false,
+      },
+    );
+  });
+
+  test('Should call api create new list when onSave if has not listId', async () => {
+    mockParams.listId = null;
+    let spy = jest.spyOn(axios, 'post');
+
+    const {result} = renderHook(() => useListForm(), {
+      wrapper: Wrapper,
+    });
+
+    act(() => {
+      result.current.onChangeForm('Test', 'name');
+    });
+
+    await act(async () => {
+      await result.current.onSave();
+    });
+
+    expect(mockGetListById).toHaveBeenCalledWith(1);
+    expect(spy).toHaveBeenCalledWith(
+      `${CollectionListService.baseURL}/4/list`,
+      {
+        description: '',
+        iso_3166_1: 'US',
+        iso_639_1: 'en',
+        name: 'Test',
+        public: false,
+      },
+    );
+  });
 });
