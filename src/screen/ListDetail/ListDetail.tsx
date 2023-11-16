72c0e43034ad4b3543ea41074ad1d4d733e01a7f
komang <komang@farmacare.id>
2023-11-16 18:01:09 +0800

    tweak: cleanup

--- src/screen/ListDetail/ListDetail.tsx
+++ src/screen/ListDetail/ListDetail.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import {Alert, FlatList, Text, View} from 'react-native';
+
+// hooks
 import {useListDetail} from './useListDetail';
+
+// component
 import {CardRowItem} from '../../components/CardRowItem';
 import {Layout} from '../../elements/Layout';
 import {Label} from '../../elements/Label';
@@ -18,75 +22,73 @@ export const ListDetail = () => {
     onNavigateToEditList,
   } = useListDetail();
   return (
-    <>
-      <Layout>
-        <TopBar
-          actionBar={
-            <View style={{flexDirection: 'row'}}>
-              <Button
-                title="Delete"
-                loading={loading}
-                type="danger"
-                onPress={() =>
-                  Alert.alert(`Delete list ${listDetail?.name}`, '', [
-                    {
-                      text: 'Cancel',
-                      onPress: () => console.log('Cancel Pressed'),
-                      style: 'cancel',
-                    },
-                    {
-                      text: 'Delete',
-                      onPress: () => onDeleteList(),
-                    },
-                  ])
-                }
-              />
-              <Button
-                title="Edit"
-                style={{marginLeft: 4}}
-                onPress={onNavigateToEditList}
-              />
-            </View>
-          }
-        />
-
-        <FlatList
-          ListHeaderComponent={
-            <>
-              <HeaderWrapper>
-                <Label size="header">{listDetail?.name}</Label>
-                <Text>{listDetail?.description}</Text>
-              </HeaderWrapper>
-              <Label size="lg">Movie list</Label>
-            </>
-          }
-          ListEmptyComponent={
-            <Placeholder>
-              <Label size="md">Data movie list is empty</Label>
-            </Placeholder>
-          }
-          data={listDetail?.results || []}
-          renderItem={movie => (
-            <CardRowItem
-              imageUri={movie.item.poster_path}
-              onPress={() => onNavigateToMovieDetail(movie.item.id)}
-              content={
-                <>
-                  <Label size="md" numberOfLines={1} ellipsizeMode="tail">
-                    {movie.item.title}
-                  </Label>
-                  <Text
-                    numberOfLines={1}
-                    ellipsizeMode="tail"
-                    style={{color: '#FF9209'}}>
-                    Rating {movie.item.vote_average}
-                  </Text>
-                </>
+    <Layout>
+      <TopBar
+        actionBar={
+          <View style={{flexDirection: 'row'}}>
+            <Button
+              title="Delete"
+              loading={loading}
+              type="danger"
+              onPress={() =>
+                Alert.alert(`Delete list ${listDetail?.name}`, '', [
+                  {
+                    text: 'Cancel',
+                    onPress: () => console.log('Cancel Pressed'),
+                    style: 'cancel',
+                  },
+                  {
+                    text: 'Delete',
+                    onPress: () => onDeleteList(),
+                  },
+                ])
               }
             />
-          )}
-        />
-      </Layout>
-    </>
+            <Button
+              title="Edit"
+              style={{marginLeft: 4}}
+              onPress={onNavigateToEditList}
+            />
+          </View>
+        }
+      />
+
+      <FlatList
+        ListHeaderComponent={
+          <>
+            <HeaderWrapper>
+              <Label size="header">{listDetail?.name}</Label>
+              <Text>{listDetail?.description}</Text>
+            </HeaderWrapper>
+            <Label size="lg">Movie list</Label>
+          </>
+        }
+        ListEmptyComponent={
+          <Placeholder>
+            <Label size="md">Data movie list is empty</Label>
+          </Placeholder>
+        }
+        data={listDetail?.results || []}
+        renderItem={movie => (
+          <CardRowItem
+            imageUri={movie.item.poster_path}
+            onPress={() => onNavigateToMovieDetail(movie.item.id)}
+            content={
+              <>
+                <Label size="md" numberOfLines={1} ellipsizeMode="tail">
+                  {movie.item.title}
+                </Label>
+                <Text
+                  numberOfLines={1}
+                  ellipsizeMode="tail"
+                  style={{color: '#FF9209'}}>
+                  Rating {movie.item.vote_average}
+                </Text>
+              </>
+            }
+          />
+        )}
+      />
+    </Layout>
   );
 };
